4cfb787d5993c03214c689a729494913a79f3e1a
Peter Matthias <32749390+PeterMatthias@users.noreply.github.com>
2022-12-02 21:47:00 +0100

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -5,3 +5,12 @@ wlan_pwd = "YOUR_WLAN_PASSWORD"
 
 lat = 0 # your location latitude
 lon = 0 # your location longitude
+
+#lat = 55.75		# Moskau
+#lon = 37.616667
+
+#lat = 39.933333	# Peking
+#lon = 116.383333
+
+#lat = 21.311389		# Hawaii
+#lon = -157.796389
